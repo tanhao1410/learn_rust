d816807cf4d020a146f772162a872205bc6590b0
tanhao <tanhao@wondersgroup.com>
2020-10-27 16:50:58 +0800

    202. 快乐数

--- day_1026/src/main.rs
+++ day_1026/src/main.rs
@@ -2,35 +2,52 @@ use std::cell::RefCell;
 use std::rc::{Rc, Weak};
 use std::borrow::Borrow;
 
-
+pub fn is_happy(n: i32) -> bool {
+    use std::collections::HashSet;
+    let mut m = HashSet::new();
+    let mut nn = n;
+    loop {
+        if m.get(&nn).is_some() {
+            return false;
+        } else {
+            m.insert(nn);
+        }
+        let mut sum = 0;
+        while nn != 0 {
+            sum += ((nn % 10) * (nn % 10));
+            nn = nn / 10;
+        }
+        if sum == 1 {
+            return true;
+        }
+    }
+}
 
 #[derive(Debug, PartialEq, Eq)]
 pub struct TreeNode {
-  pub val: i32,
-  pub left: Option<Rc<RefCell<TreeNode>>>,
-  pub right: Option<Rc<RefCell<TreeNode>>>,
+    pub val: i32,
+    pub left: Option<Rc<RefCell<TreeNode>>>,
+    pub right: Option<Rc<RefCell<TreeNode>>>,
 }
 
 impl TreeNode {
-  #[inline]
-  pub fn new(val: i32) -> Self {
-    TreeNode {
-      val,
-      left: None,
-      right: None
+    #[inline]
+    pub fn new(val: i32) -> Self {
+        TreeNode {
+            val,
+            left: None,
+            right: None,
+        }
     }
-  }
 }
 
-struct Solution{
-
-}
+struct Solution {}
 
 impl Solution {
     pub fn inorder_traversal(root: Option<Rc<RefCell<TreeNode>>>) -> Vec<i32> {
         //递归方式
         let mut res = vec![];
-        if let Some(node) = root{
+        if let Some(node) = root {
             let mut vec1 = Solution::inorder_traversal(node.borrow_mut().left.clone());
             res.append(&mut vec1);
             res.push(node.borrow_mut().val);
@@ -43,31 +60,38 @@ impl Solution {
 
 
 fn main() {
-    let nums = vec![1,2,3,4];
-    let mut list = create_list(nums);
+    // let nums = vec![1,2,3,4];
+    // let mut list = create_list(nums);
+    //
+    // let mut head = &list;
+    //
+    // while head.is_some(){
+    //     println!("{}",head.as_ref().unwrap().val);
+    //     head = &head.as_ref().unwrap().next
+    // }
 
-    let mut head = &list;
-
-    while head.is_some(){
-        println!("{}",head.as_ref().unwrap().val);
-        head = &head.as_ref().unwrap().next
-    }
-
+    f1(S { val: 1 })
 }
+
+struct S {
+    val: i32,
+}
+
+fn f1(ref s: S) {}
 
 #[derive(Debug)]
 struct Node {
     val: i32,
-    next: Option<Box<Node>>
+    next: Option<Box<Node>>,
 }
 
 impl Node {
-    pub fn new(num: i32) -> Node{
-       Node{val:num,next:None}
+    pub fn new(num: i32) -> Node {
+        Node { val: num, next: None }
     }
 }
 
-fn create_list(v :Vec<i32>)->Option<Box<Node>>{
+fn create_list(v: Vec<i32>) -> Option<Box<Node>> {
     if v.is_empty() {
         return None;
     }
